4d218c15f860895cf47449b9b6a7fc50ff762583
wai-1002 <wai.create2021@gmail.com>
2023-01-14 11:30:12 +0900

    Initial Commit

---
+++ Product/ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductApp.swift
+//  Product
+//
+//  Created by 若生優希 on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
