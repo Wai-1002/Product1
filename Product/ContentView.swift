633c68d660b00829cf249482182012a5643c38a2
wai-1002 <wai.create2021@gmail.com>
2023-01-15 12:38:52 +0900

    せんぱ〜い

--- Product/ContentView.swift
+++ Product/ContentView.swift
@@ -26,6 +26,26 @@ struct ContentView: View {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct CardModalView: View {
     @State private var offsets = (top: CGFloat.zero, middle: CGFloat.zero, bottom: CGFloat.zero)
     @State private var offset: CGFloat = .zero
